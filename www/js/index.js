32aa43772ed69bc83e4998907c4c45ec46ddf512
unknown <meddah.abdellah.spcx@gmail.com>
2020-01-14 18:25:27 +0100

    origin

--- www/js/index.js
+++ www/js/index.js
@@ -154,10 +154,12 @@ var app = {
           }
        },
        serialDataCallback : function(rawData){
-        console.log(rawData);
-        alert(rawData);
-        var data = rawData.split(',');
-        if(data.length>=5 && data.length<=6)app.addInfo(data[0],data[1],data[2],data[3],data[4]);
+        if(app.root==3){
+          console.log(rawData);
+          //alert(rawData);
+          var data = rawData.split(',');
+          if(data.length>=5 && data.length<=6)app.addInfo(data[0],data[1],data[2],data[3],data[4]);
+        }
        },
        writeSerial : function(data){
          serial.write(data, function(){}, function(){alert("couldn't send");app.startSerial();});
