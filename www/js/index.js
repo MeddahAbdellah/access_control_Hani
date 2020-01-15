40d243473ac9e628a65fd1527f0ab8c8512b9b06
unknown <meddah.abdellah.spcx@gmail.com>
2020-01-15 18:04:43 +0100

    master

--- www/js/index.js
+++ www/js/index.js
@@ -156,6 +156,7 @@ var app = {
        serialDataCallback : function(rawData){
         if(app.page==3){
           console.log(rawData);
+          $(".container").append(rawData+"<br>")
           var data = rawData.split(',');
           if(data.length>=5 && data.length<=6)app.addInfo(data[0],data[1],data[2],data[3],data[4]);
         }
